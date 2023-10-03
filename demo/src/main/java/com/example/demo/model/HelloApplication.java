ae7bb224c1b1cac439af1cf67cfd8e03e94cf966
Angel-L-G <angelchiquitopons@gmail.com>
2023-10-03 19:17:18 +0100

    feat: mvc done and base files too;

--- demo/src/main/java/com/example/demo/model/HelloApplication.java
+++ demo/src/main/java/com/example/demo/model/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.model;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -18,6 +18,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
